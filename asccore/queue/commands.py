29b2ea6198c2830ae508f706cacedb32878fd5eb
drabbit <d@rabbit.com>
2024-05-26 17:50:29 +0300

    fix

--- asccore/queue/commands.py
+++ asccore/queue/commands.py
@@ -3,9 +3,7 @@ from asccore.queue import AscWorker
 
 from redis import Redis
 
-from asccore.contracts.kernel import Kernel
 from asccore.main import app
-from app.entity.channels import Post
 
 @cli.command("queue.work")
 def package_test():
